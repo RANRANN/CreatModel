c47e34ce62283476c3224437f6d7d51b8f7f208f
a.ryzhakov2011@yandex.ru <a.ryzhakov2011@yandex.ru>
2022-05-05 22:17:36 +0300

    Добавьте файлы проекта.

---
+++ CreatModel.cs
@@ -0,0 +1,89 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreatModel
+{
+    public class CreatModel
+    {
+        [Transaction(TransactionMode.Manual)]
+        public class CreationModel : IExternalCommand
+        {
+            public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+            {
+                //Document doc = commandData.Application.ActiveUIDocument.Document;
+                //var res1 = new FilteredElementCollector(doc)
+                //    .OfClass(typeof(WallType))
+                //    //.Cast<Wall>()
+                //    .OfType<WallType>()
+                //    .ToList();
+
+                //var res2 = new FilteredElementCollector(doc)
+                //    .OfClass(typeof(FamilyInstance))
+                //    //.Cast<Wall>()
+                //    .OfType<FamilyInstance>()
+                //    .ToList();
+
+                //return Result.Succeeded;
+
+                Document doc = commandData.Application.ActiveUIDocument.Document;
+
+                Level level1, level2;
+                TakeLevels(doc, out level1, out level2);
+
+                CreateWalls(doc, level1, level2);
+
+                return Result.Succeeded;
+            }
+
+            private static void CreateWalls(Document doc, Level level1, Level level2)
+            {
+                double width = UnitUtils.ConvertToInternalUnits(10000, DisplayUnitType.DUT_MILLIMETERS);
+                double depth = UnitUtils.ConvertToInternalUnits(5000, DisplayUnitType.DUT_MILLIMETERS);
+                double dx = width / 2;
+                double dy = depth / 2;
+
+                List<XYZ> points = new List<XYZ>();
+                points.Add(new XYZ(-dx, -dy, 0));
+                points.Add(new XYZ(dx, -dy, 0));
+                points.Add(new XYZ(dx, dy, 0));
+                points.Add(new XYZ(-dx, dy, 0));
+                points.Add(new XYZ(-dx, -dy, 0));
+
+                List<Wall> walls = new List<Wall>();
+
+                Transaction ts = new Transaction(doc, "Создание стены");
+                ts.Start();
+                for (int i = 0; i < 4; i++)
+                {
+                    Line line = Line.CreateBound(points[i], points[i + 1]);
+                    Wall wall = Wall.Create(doc, line, level1.Id, false);
+                    walls.Add(wall);
+                    wall.get_Parameter(BuiltInParameter.WALL_HEIGHT_TYPE).Set(level2.Id);
+                }
+                ts.Commit();
+            }
+
+            private static void TakeLevels(Document doc, out Level level1, out Level level2)
+            {
+                List<Level> listLevel = new FilteredElementCollector(doc)
+                    .OfClass(typeof(Level))
+                    .OfType<Level>()
+                    .ToList();
+
+                level1 = listLevel
+                    .Where(x => x.Name.Equals("1-ый этаж"))
+                    .FirstOrDefault();
+                level2 = listLevel
+                    .Where(x => x.Name.Equals("2-ой этаж"))
+                    .FirstOrDefault();
+
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("CreatModel")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CreatModel")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("19ad7623-9342-4a39-ba85-7c694e91940c")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
